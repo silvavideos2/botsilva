ab56d51ac698db6e01da8ef6619d54259178bb95
silvavideos2 <vitor0119n@gmail.com>
2021-08-11 17:56:27 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,39 @@
+const help = (prefix) => {
+return`
+╔┬━━━━━━━━━━━┈ ❄︎
+││                     Silva Bot      
+││⇢ᴅᴏɴᴏ: SilvaVideosYt
+││⇢ɴᴜᴍᴇʀᴏ: *wa.me/556199730280*
+││⇢ɪɴsᴛᴀ:@silva_videos_yt
+││⇢ᴄᴀɴᴀʟ:↓↓↓
+││→https://youtube.com/c/SilvaVideosYt1
+││⇢ᴘɪx:EM BREVE PONHO
+││⇢ᴘʀᴇғɪx: ⌈${prefix}⌋
+││⇢𝚒𝚗𝚏𝚘 𝚌𝚘𝚟𝚒𝚍-19: ⌈${prefix}covid19⌋
+││⇢𝚌𝚊𝚜𝚘 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚎 𝚎𝚛𝚛𝚘𝚜: ⌈${prefix}bugreport⌋
+│╰────────────┈ ❄︎
+ ⊢───◣SILVA BOT◥───✯
+│
+│⇨ *${prefix}menufig*
+│⚠️ menu de criação de stickers
+│⇨ *${prefix}plays*
+│⚠️ plays de musica
+│Downloads de audios do YT
+│⇨ *${prefix}jogos*
+│⚠️ comandos de jogos 
+│para zoar com amigos.
+│⇨ *${prefix}menuadm* 
+│ ⚠️ administração de grupos
+│⇨ *${prefix}silva*
+│ ⚠️ comandos que so o dono do bot pode usar 
+│
+╰────⟪silva⟫────┈✯
+©     _*Copyright by silva*_
+®     _*dominando tudo⚡*_ 
+╘────────────💫`
+
+}
+
+
+
+exports.help = help
---
+++ src/skiller.js
@@ -0,0 +1,24 @@
+const skiller = (prefix) => {
+	return `
+╭───────────⌁
+│  🧐🥶 _*silva bot*_ 🥶🧐
+╘┬──────────⌁
+┌┤ 🥵 _*silva*_🥵
+││
+││  *${prefix}aviso*
+││⚠️ para fazer um aviso
+││  *${prefix}divulgar*
+││⚠️ de aviso de videos
+││  *${prefix}bc*
+││⚠️ fazer tm para  todos os usuários
+││  *${prefix}setprefix*
+││⚠️ mudar o prefixo do bot
+││  *${prefix}hidetag*
+││⚠️ marcação invisível
+││
+│╰──────────┈┈💫
+│             _*Silva bot*_
+╰───────────┈┈💫`
+}
+
+exports.skiller = skiller
